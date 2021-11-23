aa5cd050a873054618336d9815bee1cd5bb20569
Fidelis <fideliselias95@gmail.com>
2021-11-22 22:14:35 -0300

    Crud funcionario

---
+++ src/main/java/com/br/desafio/controller/FuncionarioController.java
@@ -0,0 +1,44 @@
+package com.br.desafio.controller;
+
+import com.br.desafio.dto.DepartamentoDTO;
+import com.br.desafio.dto.FuncionarioDTO;
+import com.br.desafio.service.DepartamentoService;
+import com.br.desafio.service.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/funcionario")
+public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioService service;
+
+    @PostMapping
+    public ResponseEntity<FuncionarioDTO> criar(@RequestBody FuncionarioDTO funcionarioDto) {
+        FuncionarioDTO funcionario = service.criar(funcionarioDto);
+        return ResponseEntity.ok(funcionario);
+    }
+
+    @PutMapping
+    public ResponseEntity<FuncionarioDTO> atualizar(@RequestBody FuncionarioDTO funcionarioDto) {
+        FuncionarioDTO funcionario = service.atualizar(funcionarioDto);
+        return ResponseEntity.ok(funcionario);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<FuncionarioDTO>> listar() {
+        List<FuncionarioDTO> funcionarios = service.listar();
+        return ResponseEntity.ok(funcionarios);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable("id") Long id) {
+        service.remover(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
--- src/main/java/com/br/desafio/service/FuncionarioService.java
+++ src/main/java/com/br/desafio/service/FuncionarioService.java
@@ -1,23 +1,73 @@
 package com.br.desafio.service;
 
+import com.br.desafio.domain.Departamento;
+import com.br.desafio.domain.Funcionario;
+import com.br.desafio.dto.FuncionarioDTO;
+import com.br.desafio.mapper.FuncionarioMapper;
 import com.br.desafio.repository.FuncionarioRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class FuncionarioService {
 
     private final FuncionarioRepository repository;
 
-    public FuncionarioService(FuncionarioRepository repository) {
+    private final FuncionarioMapper mapper;
+
+    public FuncionarioService(FuncionarioRepository repository, FuncionarioMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
-    public void criar() {}
+    public FuncionarioDTO criar(FuncionarioDTO funcionarioDto) {
+        Funcionario funcionario = mapper.toEntity(funcionarioDto);
 
-    public void atualizar() {}
+        repository.save(funcionario);
 
-    public void listar() {}
+        return mapper.toDto(funcionario);
+    }
 
-    public void remover() {}
+    public FuncionarioDTO atualizar(FuncionarioDTO funcionarioDto) {
+        Optional<Funcionario> funcionarioOp = repository.findById(funcionarioDto.getId());
+
+        if (funcionarioOp.isPresent()) {
+            Funcionario funcionario = funcionarioOp.get();
+            funcionario.setNome(funcionarioDto.getNome());
+            funcionario.setCpf(funcionarioDto.getCpf());
+            funcionario.setDataNascimento(funcionarioDto.getDataNascimento());
+            funcionario.setSexo(funcionarioDto.getSexo());
+
+            if (Objects.nonNull(funcionarioDto.getEndereco())) {
+                funcionario.getEndereco().setPais(funcionarioDto.getEndereco().getPais());
+                funcionario.getEndereco().setUf(funcionarioDto.getEndereco().getUf());
+                funcionario.getEndereco().setCidade(funcionarioDto.getEndereco().getCidade());
+                funcionario.getEndereco().setCep(funcionarioDto.getEndereco().getCep());
+            }
+
+            if (Objects.nonNull(funcionarioDto.getSupervisor())) {
+                funcionario.getSupervisor().setId(funcionarioDto.getSupervisor().getId());
+            }
+
+            repository.save(funcionario);
+
+            funcionarioDto = mapper.toDto(funcionario);
+        }
+
+        return funcionarioDto;
+    }
+
+    public List<FuncionarioDTO> listar() {
+        List<Funcionario> funcionarios = repository.findAll();
+
+        return mapper.toDto(funcionarios);
+    }
+
+    public void remover(Long id) {
+        repository.deleteById(id);
+    }
 
 }
