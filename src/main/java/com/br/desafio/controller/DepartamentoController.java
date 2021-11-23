e5cb3c844659b60f8a72be2029f93a145f64bdd9
Fidelis <fideliselias95@gmail.com>
2021-11-22 21:39:34 -0300

    Crud departamento

--- src/main/java/com/br/desafio/controller/DepartamentoController.java
+++ src/main/java/com/br/desafio/controller/DepartamentoController.java
@@ -1,14 +1,12 @@
 package com.br.desafio.controller;
 
-import com.br.desafio.domain.Departamento;
 import com.br.desafio.dto.DepartamentoDTO;
 import com.br.desafio.service.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/departamento")
@@ -18,9 +16,27 @@ public class DepartamentoController {
     private DepartamentoService service;
 
     @PostMapping
-    public ResponseEntity<Departamento> criar(@RequestBody DepartamentoDTO departamentoDto) {
-        Departamento departamento = service.criar(departamentoDto);
+    public ResponseEntity<DepartamentoDTO> criar(@RequestBody DepartamentoDTO departamentoDto) {
+        DepartamentoDTO departamento = service.criar(departamentoDto);
         return ResponseEntity.ok(departamento);
+    }
+
+    @PutMapping
+    public ResponseEntity<DepartamentoDTO> atualizar(@RequestBody DepartamentoDTO departamentoDto) {
+        DepartamentoDTO departamento = service.atualizar(departamentoDto);
+        return ResponseEntity.ok(departamento);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<DepartamentoDTO>> listar() {
+        List<DepartamentoDTO> departamentos = service.listar();
+        return ResponseEntity.ok(departamentos);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable("id") Long id) {
+        service.remover(id);
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/com/br/desafio/service/DepartamentoService.java
+++ src/main/java/com/br/desafio/service/DepartamentoService.java
@@ -6,6 +6,9 @@ import com.br.desafio.mapper.DepartamentoMapper;
 import com.br.desafio.repository.DepartamentoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class DepartamentoService {
 
@@ -18,15 +21,37 @@ public class DepartamentoService {
         this.mapper = mapper;
     }
 
-    public Departamento criar(DepartamentoDTO departamentoDto) {
+    public DepartamentoDTO criar(DepartamentoDTO departamentoDto) {
         Departamento departamento = mapper.toEntity(departamentoDto);
-        return departamento;
+        repository.save(departamento);
+
+        return mapper.toDto(departamento);
     }
 
-    public void atualizar() {}
+    public DepartamentoDTO atualizar(DepartamentoDTO departamentoDto) {
+        Optional<Departamento> departamentoOp = repository.findById(departamentoDto.getId());
 
-    public void listar() {}
+        if (departamentoOp.isPresent()) {
+            Departamento departamento = departamentoOp.get();
+            departamento.setNome(departamentoDto.getNome());
+            departamento.setNumero(departamentoDto.getNumero());
 
-    public void remover() {}
+            repository.save(departamento);
+
+            departamentoDto = mapper.toDto(departamento);
+        }
+
+        return departamentoDto;
+    }
+
+    public List<DepartamentoDTO> listar() {
+        List<Departamento> departamentos = repository.findAll();
+
+        return mapper.toDto(departamentos);
+    }
+
+    public void remover(Long id) {
+        repository.deleteById(id);
+    }
 
 }
